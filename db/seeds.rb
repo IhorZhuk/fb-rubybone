875ce31ed1ff84be3282ff7a77bc6f6daeda0b17
IhorZhuk <ihor.zhuk92@gmail.com>
2016-12-12 14:09:25 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Transaction.create!(date: '2016-11-05', title: 'Dummy transaction title', amount: 10.5, kind: 'debit', currency: 'EUR', note: 'Dummy transaction note lorem ipsum dolor sit ammet')
-Transaction.create!(date: '2016-11-05', title: 'Dummy transaction title', amount: 1200, kind: 'credit', currency: 'EUR', note: 'Dummy transaction note lorem ipsum dolor sit ammet')
-Transaction.create!(date: '2016-11-05', title: 'Dummy transaction title', amount: 70.5, kind: 'debit', currency: 'EUR', note: 'Dummy transaction note lorem ipsum dolor sit ammet')
+now = Time.new
+
+Category.create!(title: 'Uncategorized')
+
+Transaction.create!(date: now, title: 'Dummy transaction title', amount: 10.5, kind: 'debit', currency: 'EUR', note: 'Dummy transaction note lorem ipsum dolor sit ammet', category_id: 1)
+Transaction.create!(date: now, title: 'Dummy transaction title', amount: 1200, kind: 'credit', currency: 'EUR', note: 'Dummy transaction note lorem ipsum dolor sit ammet', category_id: 1)
+Transaction.create!(date: now, title: 'Dummy transaction title', amount: 70.5, kind: 'debit', currency: 'EUR', note: 'Dummy transaction note lorem ipsum dolor sit ammet',  category_id: 1)
