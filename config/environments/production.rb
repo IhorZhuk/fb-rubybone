fc325d957b55c5c09e99fdb9343ca1bbbd4019ef
IhorZhuk <ihor.zhuk92@gmail.com>
2017-01-04 21:03:27 +0100

    add mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,19 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.delivery_method = :smtp
+    # SMTP settings for gmail
+    config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['MAILER_MAIL'],
+    :password             => ENV['MAILER_PASSWORD'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+
+  config.action_mailer.default_url_options = { :host => "fb-rubybone.herokuapp.com" }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -59,7 +72,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
