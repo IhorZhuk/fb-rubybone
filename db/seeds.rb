7f46fffd71e5d0343b10cb050127a2bce833114b
IhorZhuk <ihor.zhuk92@gmail.com>
2017-01-14 19:30:27 +0100

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,15 @@
-user_id = 15
-category_id = 16
+user_id = 1
+category_id = 1
 
 for i in (1..55)
   kind = ['credit', 'debit']
 
   Transaction.create!(
     user_id: user_id,
-    date: Faker::Date.between(60.days.ago, Date.today), 
+    date: Faker::Date.between(35.days.ago, Date.today), 
     title: Faker::Lorem.sentence(2), 
     amount: rand(10..1000), 
     kind: kind.sample, 
-    currency: 'EUR', 
     note: Faker::Lorem.sentences.join(" "), 
     category_id: category_id
   )
